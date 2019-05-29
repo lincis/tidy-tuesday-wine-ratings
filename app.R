b7489b6e02731026f4f940da05a4b1524b28c767
Linards Kalvans <linards.kalvans@gmail.com>
2019-05-29 23:46:22 +0300

    Improve shinyapp for mobile devices

--- app.R
+++ app.R
@@ -2,7 +2,7 @@ library(shiny)
 library(magrittr)
 library(dplyr)
 library(tidyr)
-library(tagcloud)
+library(wordcloud)
 library(RColorBrewer)
 
 wine.ratings <- readRDS("wine.ratings.rds")
@@ -16,32 +16,38 @@ ui <- fluidPage(
   # App title ----
   titlePanel("How words in wine description match the wine quality")
 
-  # Sidebar panel for inputs ----
-  , sidebarLayout(
-    sidebarPanel(
-      tags$head(tags$script('$(document).on("shiny:connected", function(e) {
-                            Shiny.onInputChange("innerWidth", window.innerWidth);
-                            });
-                            $(window).resize(function(e) {
-                            Shiny.onInputChange("innerWidth", window.innerWidth);
-                            });
-                            '))
-      # Input: Select rating range
-      , sliderInput("points.range", "Wine rating range (relative)", min = 0, max = 100, value = c(90, 100))
-      # Number of words in cloud
-      , sliderInput("num.words", "Number of words to display in cloud", min = 10, max = 200, value = 50, step = 1)
-      # Filter out most frequent words
-      , sliderInput(
-        "words.freq.cutoff"
-        , "Words that occur in too many descriptions (percent of them) are excluded from cloud"
-        , min = 0, max = 100, value = 20)
-    )
-    , mainPanel(
-      fillPage(
-        div("The word cloud below shows the most frequent words used to describe the selected fraction of wines in dataset.")
-        , textOutput("points.range")
-        , plotOutput("tag.cloud")
+  , fluidRow(
+    
+    column(
+      4
+      , wellPanel(
+        tags$head(tags$script('
+$(document).on("shiny:connected", function(e) {
+  Shiny.onInputChange("innerWidth", window.innerWidth);
+});
+$(window).resize(function(e) {
+  Shiny.onInputChange("innerWidth", window.innerWidth);
+});
+'))
+        # Input: Select rating range
+        , sliderInput("points.range", "Wine rating range (relative)", min = 0, max = 100, value = c(90, 100))
+        # Number of words in cloud
+        , sliderInput("num.words", "Number of words to display in cloud", min = 10, max = 200, value = 50, step = 1)
+        # Filter out most frequent words
+        , sliderInput(
+          "words.freq.cutoff"
+          , "Words that occur in too many descriptions (percent of them) are excluded from cloud"
+          , min = 0, max = 100, value = 20
+        )
       )
+    ),
+    
+    column(
+      8
+      , id = "main"
+      , div("The word cloud below shows the most frequent words used to describe the selected fraction of wines in dataset.")
+      , textOutput("points.range")
+      , plotOutput("tag.cloud", width = "auto", height = "auto")
     )
   )
 )
@@ -67,9 +73,27 @@ server <- function(input, output) {
   })
   output$tag.cloud <- renderPlot(
     {
-      tagcloud(names(count.words()) %>% tail(input$num.words), count.words() %>% tail(input$num.words), col = colors())
+      wordcloud(
+        names(count.words()) %>% tail(input$num.words)
+        , count.words() %>% tail(input$num.words)
+        , colors = colors()
+        , scale =       case_when(
+          is.null(input[["innerWidth"]]) ~ c(4, .5)
+          , input[["innerWidth"]] < 768  ~ input[["innerWidth"]] / c(167, 1067)
+          , TRUE                         ~ input[["innerWidth"]] / c(200, 1600)
+        )
+        
+      )
     }
-    , height = reactive(ifelse(!is.null(input$innerWidth), input$innerWidth * .5, 0))
+    # , height = reactive(ifelse(!is.null(input$innerWidth), (input$innerWidth * .8) %>% as.character(), "auto"))
+    , height = reactive(
+      case_when(
+        is.null(input[["innerWidth"]]) ~ 0
+        , input[["innerWidth"]] < 768  ~ input$innerWidth * .9
+        , input[["innerWidth"]] < 992  ~ input$innerWidth * .55
+        , TRUE                         ~ input$innerWidth * .55
+      )
+    )
   )
 
 }
